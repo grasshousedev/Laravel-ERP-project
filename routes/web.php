ce694b76e5428c0b56032991a9996a69cb84e0eb
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-10-10 13:33:37 -0500

    index

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
+    return view('admin.index');
+})->name('admin');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
