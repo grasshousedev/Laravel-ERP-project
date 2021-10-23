19c33d136ec1d2558283f7356cc0ecd730fbc83b
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-10-22 21:59:56 -0500

    tabla no creisi

--- resources/views/admin/ventas/parts/form_tabla.blade.php
+++ resources/views/admin/ventas/parts/form_tabla.blade.php
@@ -4,8 +4,7 @@
             <th scope="col">Item</th>
             <th scope="col">Cantidad</th>
             <th scope="col">Precio Venta</th>
-            <th scope="col">Total</th>
-            <th scope="col" colspan="2">Precio Neto</th>
+            <th scope="col" colspan="2">Total</th>
         </tr>
     </thead>
     <tbody id="table_data">
@@ -13,9 +12,8 @@
             <th scope="row"><input class="bg-dark text-white" type="text" id="item"></th>
             <th scope="row"><input class="bg-dark text-white" type="number" id="cantidad"></th>
             <th scope="row"><input class="bg-dark text-white" type="number" id="precio_venta"></th>
-            <th scope="row"><input class="bg-dark text-white" type="number" id="total"></th>
             <th scope="row">
-                <input class="bg-dark text-white" type="number" id="precio_neto">
+                <input class="bg-dark text-white border-0" type="number" id="total" readonly>
                 <a class="btn btn-primary" href="#" role="button" onclick="add_row()">Item <i class="far fa-plus-square"></i> </a>
             </th>
         </tr>
@@ -56,8 +54,7 @@
         const item = document.getElementById("item");
         const cant = document.getElementById("cantidad");
         const pventa = document.getElementById("precio_venta");
-        const total = document.getElementById("total");
-        const pneto = document.getElementById("precio_neto");
+        const total = parseFloat(document.getElementById("cantidad").value) * parseFloat(document.getElementById("precio_venta").value);
 
         if (item.value != "") {
             const table = document.getElementsByTagName("table")[0];
@@ -66,18 +63,17 @@
             new_row.insertCell(0).innerHTML = item.value;
             new_row.insertCell(1).innerHTML = cant.value;
             new_row.insertCell(2).innerHTML = pventa.value;
-            new_row.insertCell(3).innerHTML = total.value;
-            new_row.insertCell(4).innerHTML = pneto.value;
+            new_row.insertCell(3).innerHTML = total;
 
             // TOTALES
-            let sub_total = parseFloat(total.value);
-            let igv = calc_igv(total.value, 10);
+            let sub_total = total;
+            let igv = calc_igv(total, 10);
             let neto = sub_total - igv;
 
             if (document.getElementById("sub_total").value != "") {
 
-                sub_total = parseFloat(document.getElementById("sub_total").value) + parseFloat(total.value);
-                igv = parseFloat(document.getElementById("igv").value) + calc_igv(total.value, 10);
+                sub_total = parseFloat(document.getElementById("sub_total").value) + parseFloat(total);
+                igv = parseFloat(document.getElementById("igv").value) + calc_igv(total, 10);
                 neto = parseFloat(document.getElementById("neto").value) + (sub_total - igv);
 
             }
@@ -90,7 +86,6 @@
             item.value = "";
             cant.value = "";
             pventa.value = "";
-            total.value = "";
             pneto.value = "";
         }
     }
