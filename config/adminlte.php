88adc2e421eb344c71e648779b3257292a7a4dd8
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-10-26 20:20:35 -0500

    adminlte fixes

--- config/adminlte.php
+++ config/adminlte.php
@@ -102,10 +102,10 @@ return [
     |
     */
 
-    'classes_auth_card' => 'card-outline card-primary',
+    'classes_auth_card' => 'bg-gradient-dark card-primary',
     'classes_auth_header' => '',
-    'classes_auth_body' => '',
-    'classes_auth_footer' => '',
+    'classes_auth_body' => 'bg-gradient-dark',
+    'classes_auth_footer' => 'text-center',
     'classes_auth_icon' => '',
     'classes_auth_btn' => 'btn-flat btn-primary',
 
