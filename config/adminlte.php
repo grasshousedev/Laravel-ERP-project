a3d05f6553aee079498a05d6fb5b3f34c6e50f4c
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-10-27 19:17:54 -0500

    nombre del dashboard

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>3D</b>Group <small>S.A.C.</small>',
+    'logo' => '<b>3D</b>Group',
     'logo_img' => 'vendor/adminlte/dist/img/SENATI-logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
