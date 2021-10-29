89ba1205a7f60a03da39e0ca37eb6bef0b6ec17a
Oleksandr <boltbright525@gmail.com>
2021-10-29 13:47:25 -0500

    estilo ver mas

--- resources/views/admin/clientes/ver_mas.blade.php
+++ resources/views/admin/clientes/ver_mas.blade.php
@@ -14,99 +14,107 @@
     @endif
     <div class="card">
         <div class="card-body">
-            <table class="table table-sm bg-dark">
-                <tbody>
-                    <tr>
-                        <td>
-                            <label for="ruc">Ruc</label>
-                        </td>
-                        <td>
-                            <input type="text" id="ruc" class="bg-dark text-white border-0" readonly="readonly" value="{{$clientes->ruc}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="razon_social">Razon social</label>
-                        </td>
-                        <td>
-                            <input type="text" id="razon_social" class="bg-dark text-white border-0" readonly="readonly" value="{{$clientes->razon_social}}">                            
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="direccion"> Dirección </label>
-                        </td>
-                        <td>
-                            <input type="text" id="direccion" class="bg-dark text-white border-0" readonly="readonly" value="{{$clientes->direccion}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="celular1"> Celular 1 </label>
-                        </td>
-                        <td>
-                            <input type="text" id="celular1" class="bg-dark text-white border-0" readonly="readonly" value="{{$clientes->celular1}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="celular2"> Celular 2 </label>
-                        </td>
-                        <td>
-                            <input type="text" id="celular2" class="bg-dark text-white border-0" readonly="readonly" value="{{$clientes->celular2}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email1"> Email 1 </label>
-                        </td>
-                        <td>
-                            <input type="text" id="email1" class="bg-dark text-white border-0" readonly="readonly" value="{{$clientes->email1}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email2"> Email 2 </label>
-                        </td>
-                        <td>
-                            <input type="text" id="email2" value="{{$clientes->email2}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="pagina_web"> Pagina Web </label>
-                        </td>
-                        <td>
-                            <input type="text" id="pagina_web" value="{{$clientes->paginaweb}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="area"> Area </label>
-                        </td>
-                        <td>
-                            <input type="text" id="area" value="{{$clientes->area}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="estado"> Estado </label>
-                        </td>
-                        <td>
-                            <input type="text" id="estado" value="{{$clientes->estado}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="asignado"> Asignado </label>
-                        </td>
-                        <td>
-                            <input type="text" id="asignado" value="{{$clientes->asignado}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                </tbody>
-
-            </table>
+            <div class="row row-cols-1 row-cols-md-5 g-4" style="max-width: 100rem;">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">RUC</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->ruc}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Razon Social</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->razon_social}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Direccion</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->direccion}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Celular 1</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->celular1}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Celular 2</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->celular2}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Email 1</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->email1}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Email 2</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->email2}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Pagina Web</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->paginaweb}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Area</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->area}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Estado</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->estado}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Asignado</h5>
+                            <br>
+                            <p class="card-text">{{$clientes->asignado}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <a href="{{ route('admin.clientes.index') }}" class="btn btn-sm btn-primary">Volver</a>
 
--- resources/views/admin/proveedores/ver_mas.blade.php
+++ resources/views/admin/proveedores/ver_mas.blade.php
@@ -14,99 +14,108 @@
     @endif
     <div class="card">
         <div class="card-body">
-            <table class="table table-sm bg-dark">
-                <tbody>
-                    <tr>
-                        <td>
-                            <label for="ruc">Ruc</label>
-                        </td>
-                        <td>
-                            <input type="text" id="ruc" class="bg-dark text-white border-0" readonly="readonly" value="{{$proveedores->ruc}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="razon_social">Razon social</label>
-                        </td>
-                        <td>
-                            <input type="text" id="razon_social" class="bg-dark text-white border-0" readonly="readonly" value="{{$proveedores->razon_social}}">                            
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="direccion"> Dirección </label>
-                        </td>
-                        <td>
-                            <input type="text" id="direccion" class="bg-dark text-white border-0" readonly="readonly" value="{{$proveedores->direccion}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="celular1"> Celular 1 </label>
-                        </td>
-                        <td>
-                            <input type="text" id="celular1" class="bg-dark text-white border-0" readonly="readonly" value="{{$proveedores->celular1}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="celular2"> Celular 2 </label>
-                        </td>
-                        <td>
-                            <input type="text" id="celular2" class="bg-dark text-white border-0" readonly="readonly" value="{{$proveedores->celular2}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email1"> Email 1 </label>
-                        </td>
-                        <td>
-                            <input type="text" id="email1" class="bg-dark text-white border-0" readonly="readonly" value="{{$proveedores->email1}}">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email2"> Email 2 </label>
-                        </td>
-                        <td>
-                            <input type="text" id="email2" value="{{$proveedores->email2}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="pagina_web"> Pagina Web </label>
-                        </td>
-                        <td>
-                            <input type="text" id="pagina_web" value="{{$proveedores->paginaweb}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="area"> Area </label>
-                        </td>
-                        <td>
-                            <input type="text" id="area" value="{{$proveedores->area}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="estado"> Estado </label>
-                        </td>
-                        <td>
-                            <input type="text" id="estado" value="{{$proveedores->estado}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="asignado"> Asignado </label>
-                        </td>
-                        <td>
-                            <input type="text" id="asignado" value="{{$proveedores->asignado}}" class="bg-dark text-white border-0" readonly="readonly">
-                        </td>
-                    </tr>
-                </tbody>
-
-            </table>
+            <div class="card-body">
+            <div class="row row-cols-1 row-cols-md-5 g-4" style="max-width: 100rem;">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">RUC</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->ruc}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Razon Social</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->razon_social}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Dirección</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->direccion}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Celular 1</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->celular1}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Celular 2</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->celular2}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Email 1</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->email1}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Email 2</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->email2}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Pagina Web</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->paginaweb}}></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Area</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->area}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Estado</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->estado}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Asignado</h5>
+                            <br>
+                            <p class="card-text">{{$proveedores->asignado}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <a href="{{ route('admin.proveedores.index') }}" class="btn btn-sm btn-primary">Volver</a>
 
