7bd506d45ee1f1efbfa6264434a391b2a962b6ce
Oleksandr <boltbright525@gmail.com>
2021-10-29 14:24:55 -0500

    centrar form de cotizar

--- resources/views/admin/ventas/parts/form.blade.php
+++ resources/views/admin/ventas/parts/form.blade.php
@@ -1,47 +1,46 @@
-<div class="form-row">
+<div class="form-row" style="margin-left: 10px;">
 
     {{-- @foreach ($cotizacion as $value)
     <?//php $id = $value->id; ?>
     @endforeach --}}
     <input name="codigo" type="hidden" id="validationDefault01" value="COT000">
-    <div class="input-field">
+    <div class="input-field" style="max-width: 210px;">
         {!! Form::label('cliente', 'Cliente') !!}
         {!! Form::text('cliente', null, ['class' => 'form-control', 'autocomplete'=>'off']) !!}
     </div>
-    <div class="col-md-2 my-1">
+    <div class="col-md-2 my-1" style="margin-left: 60px;">
         {!! Form::label('asignado', 'Asignado') !!}
         {!! Form::select('asignado', $users, auth()->user()->name, ['class' => 'custom-select mr-sm-2']) !!}
     </div>
 
 </div>
 
-<div class="form-row">
+<div class="form-row" style="margin-left: 10px;">
 
     <div class="col-md-1 my-1">
         {!! Form::label('cliente_id', 'Cliente ID') !!}
         {!! Form::text('cliente_id', null, ['class' => 'custom-select mr-sm-2', 'autocomplete'=>'off']) !!}
     </div>
 
-    <div class="col-md-1 my-1">
+    <div class="col-md-1 my-1" style="margin-left: 19px;">
         {!! Form::label('moneda', 'Moneda') !!}
         {!! Form::select('moneda', $moneda, null, ['class' => 'custom-select mr-sm-2']) !!}
     </div>
-
-    <div class="col-md-1.8 my-1">
+    <div class="col-md-1.8 my-1" style="margin-left: 19px;">
         {!! Form::label('tiempo_expiracion', 'Expiracion') !!}
         {!! Form::select('tiempo_expiracion', $expiracion, null, ['class' => 'custom-select mr-sm-2']) !!}
     </div>
 
 </div>
 
-<div class="form-row">
+<div class="form-row" style="margin-left: 10px;">
 
     <div class="col-md-1 my-1">
         {!! Form::label('estado', 'Estado') !!}
         {!! Form::select('estado', $estado, null, ['class' => 'custom-select mr-sm-2']) !!}
     </div>
 
-    <div class="col-auto my-1">
+    <div class="col-auto my-1" >
         {!! Form::label('forma_pago', 'Pago') !!}
         {!! Form::select('forma_pago', $forma_pago, null, ['class' => 'custom-select mr-sm-2']) !!}
     </div>
@@ -55,17 +54,17 @@
 
 <div class="form-group col-md-4">
 
-    <div class="col-auto my-1">
+    <div class="col-14 my-1">
         {!! Form::label('condiciones', 'Condiciones generales') !!}
         {!! Form::text('condiciones', null, ['class' => 'form-control', 'autocomplete'=>'off']) !!}
     </div>
 
-    <div class="col-auto my-1">
+    <div class="col-14 my-1">
         {!! Form::label('direccion', 'Direccion') !!}
         {!! Form::text('direccion', null, ['class' => 'form-control', 'autocomplete'=>'off']) !!}
     </div>
 
-    <div class="col-auto my-3">
+    <div class="col-15 my-3">
         {!! Form::label('pie_pagina', 'Pie de pagina') !!}
         {!! Form::text('pie_pagina', null, ['class' => 'form-control', 'autocomplete'=>'off']) !!}
     </div>
