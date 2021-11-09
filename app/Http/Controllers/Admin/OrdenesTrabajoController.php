225eb0748bcdf29f00decb57dd3db7fade258286
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-11-09 17:28:30 -0500

    ordenes de compra crud

--- app/Http/Controllers/Admin/OrdenesTrabajoController.php
+++ app/Http/Controllers/Admin/OrdenesTrabajoController.php
@@ -5,6 +5,18 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Ordenes_compra;
+use App\Models\estado_entrega;
+use App\Models\Forma_pago;
+use App\Models\Tiempo_entrega;
+use App\Models\Tiempo_expiracione;
+use App\Models\Tipo_moneda;
+use App\Models\User;
+use App\Models\Cotizacione;
+use App\Models\Area_provee_cliente;
+use App\Models\Cliente;
+use App\Models\Producto;
+use App\Models\Cliente_producto;
+use App\Classes\CustomCodeGenerator;
 
 class OrdenesTrabajoController extends Controller
 {
@@ -59,7 +71,15 @@ class OrdenesTrabajoController extends Controller
      */
     public function edit($id)
     {
-        //
+        $cotizacion     = Ordenes_compra::find($id);
+        $estado         = estado_entrega::pluck('estado', 'estado');
+        $forma_pago     = Forma_pago::pluck('tipo_pago', 'tipo_pago');
+        $tiempo_entrega = Tiempo_entrega::pluck('entrega', 'entrega');
+        $expiracion     = Tiempo_expiracione::pluck('expiracion', 'expiracion');
+        $moneda         = Tipo_moneda::pluck('moneda', 'moneda');
+        $users          = User::pluck('name', 'name');
+
+        return view('admin.ordenes_trabajo.edit', compact('cotizacion', 'estado', 'expiracion', 'forma_pago', 'moneda', 'tiempo_entrega', 'users'));
     }
 
     /**
@@ -71,7 +91,25 @@ class OrdenesTrabajoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'cliente'           => 'required',
+            'asignado'          => 'required',
+            'moneda'            => 'required',
+            'tiempo_expiracion' => 'required',
+            'estado'            => 'required',
+            'forma_pago'        => 'required',
+            'tiempo_entrega'    => 'required',
+            'condiciones'       => 'required',
+            'direccion'         => 'required',
+            'pie_pagina'        => 'required',
+        ]);
+
+        $datoscot = request()->except('_token', '_method', 'codigo');
+
+        Ordenes_compra::where('id', '=', $id)->update($datoscot);
+        $cotizacion = Ordenes_compra::findOrFail($id);
+
+        return redirect()->route('admin.ordenestrabajo.index', $cotizacion)->with('info', 'La orden de compra fue actualizada correctamente.');
     }
 
     /**
@@ -82,6 +120,9 @@ class OrdenesTrabajoController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Ordenes_compra::destroy($id);
+
+        $cotizacion = Ordenes_compra::all();
+        return redirect()->route('admin.ordenestrabajo.index', compact('cotizacion'))->with('info', 'La orden de compra fue eliminada correctamente.');
     }
 }
