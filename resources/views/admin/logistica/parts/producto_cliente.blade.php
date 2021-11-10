5fa691bb9dd4cc77217d494e4d1353cacf873ef9
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-11-10 15:56:50 -0500

    vista productos cliente

---
+++ resources/views/admin/logistica/parts/producto_cliente.blade.php
@@ -0,0 +1,36 @@
+<div class="productos">
+    <table class="table table-sm bg-dark" id="table_t">
+        <thead>
+            <tr>
+                <th scope="col">Item</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Precio Venta</th>
+                <th scope="col">Total</th>
+                <th scope="col">Observaciones</th>
+            </tr>
+        </thead>
+        <tbody id="table_data">
+
+            @foreach ($producto as $item)
+            <tr>
+                <th scope="row">
+                    {{ $item->producto }}
+                </th>
+                <th scope="row">
+                    {{ $item->cantidad_prod }}
+                </th>
+                <th scope="row">
+                    {{ $item->precio_prod }}
+                </th>
+                <th scope="row">
+                    {{ $item->total_prod }}
+                </th>
+                <th scope="row">
+                    {{ $item->notas }}
+                </th>
+            </tr>            
+            @endforeach
+        
+        </tbody>
+    </table>
+</div>
