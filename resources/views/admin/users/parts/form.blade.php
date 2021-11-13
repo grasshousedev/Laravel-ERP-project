8094419822d6dadd1e896f127fb9d77b8006ee95
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-11-12 22:03:49 -0500

    users migracion y insertar actualizado

--- resources/views/admin/users/parts/form.blade.php
+++ resources/views/admin/users/parts/form.blade.php
@@ -54,7 +54,7 @@
                             <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z"/>
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                         </svg>
-                        {!! Form::text('dni', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el DNI del usuario']) !!}
+                        {!! Form::text('dni', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el DNI del usuario', 'maxlength' => 8]) !!}
                         @error('dni')
                         <small class="text-danger">
                             {{ $message }}
@@ -87,17 +87,96 @@
     </div>
 </div>
 
-<div class="col-sm-10">
-    {!! Form::label('password', 'Contraseña') !!}
-    <div>
-    {!! Form::password('password', null, ['class' => 'form-control']) !!}
-       <label style="margin-left: 20px;">Debe tener entre 8 y 20 caracteres.</label>
-       @error('password')
-       <small class="text-danger">
-           {{ $message }}
-       </small>
-       @enderror
+<div class="row g-2">
+
+    <div class="col-md">
+        <div class="form-floating">
+            <div class="form-group">
+                <label class="col-md-4 control-label" >{!! Form::label('estado', 'Estado') !!}</label> 
+                <div class="col-md-11 inputGroupContainer">
+                    <div class="input-group">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16" style="margin-right: 20px;">
+                            <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                        </svg>
+                        {!! Form::text('estado', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el estado del usuario']) !!}
+                        @error('estado')
+                        <small class="text-danger">
+                            {{ $message }}
+                        </small>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <div class="col-md">
+        <div class="form-floating">
+            <div class="form-group">
+                <label class="col-md-4 control-label" >{!! Form::label('cargo', 'Cargo') !!}</label> 
+                <div class="col-md-11 inputGroupContainer">
+                    <div class="input-group">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16" style="margin-right: 20px;">
+                            <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                        </svg>
+                        {!! Form::text('cargo', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el cargo del usuario']) !!}
+                        @error('cargo')
+                        <small class="text-danger">
+                            {{ $message }}
+                        </small>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="row g-2">
+
+    <div class="col-md">
+        <div class="form-floating">
+            <div class="form-group">
+                <label class="col-md-4 control-label" >{!! Form::label('celular', 'Celular') !!}</label> 
+                <div class="col-md-11 inputGroupContainer">
+                    <div class="input-group">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16" style="margin-right: 20px;">
+                            <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                        </svg>
+                        {!! Form::text('celular', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el celular del usuario', 'maxlength' => 9]) !!}
+                        @error('celular')
+                        <small class="text-danger">
+                            {{ $message }}
+                        </small>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md">
+        <div class="form-floating">
+            <div class="form-group">
+                <label class="col-md-4 control-label" >{!! Form::label('password', 'Password') !!}</label> 
+                <div class="col-md-11 inputGroupContainer">
+                    <div class="input-group">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16" style="margin-right: 20px;">
+                            <path fill-rule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+                        </svg>
+                        {!! Form::password('password', ['class' => 'form-control']) !!}
+                        <label style="margin-left: 20px;">Debe tener entre 8 y 20 caracteres.</label>
+                        @error('password')
+                        <small class="text-danger">
+                            {{ $message }}
+                        </small>
+                        @enderror
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <input type="hidden" name="codigo" value="temporal">
 </div>
 <br>
     
