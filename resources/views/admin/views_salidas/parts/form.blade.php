a88ec09749b0f276c9a11093bcbff3354211c88c
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-11-23 20:59:36 -0500

    salida y ingreso con iconos finalizado

--- resources/views/admin/views_salidas/parts/form.blade.php
+++ resources/views/admin/views_salidas/parts/form.blade.php
@@ -448,7 +448,7 @@
                       <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
                       <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
                     </svg>
-                        <input type="time" name="hora_salida" id="hora_salida" class="form-control" min="09:00" max="20:00">
+                        <input type="time" name="hora_salida" id="hora_salida" class="form-control">
                     </div>
                 </div>
             </div>
